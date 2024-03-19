fc31373beb02fe7fe0303d44b1dd344f5b6853c8
Lulama Komana <lulama.komana@rain.co.za>
2024-03-19 17:27:19 +0200

    login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,13 @@ import { CoreState } from './store/core/core.state';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(NgxsModule.forRoot([CoreState])), 
-      provideHttpClient(),
+      provideHttpClient(), provideAnimationsAsync(),
       
   ]
 };
